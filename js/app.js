cb506cbf14ad6ad7ed0cb264607fff684f01df4f
adamilnicki <adamilnicki00@gmail.com>
2021-08-27 12:49:45 +0200

    my version

--- js/app.js
+++ js/app.js
@@ -1,28 +1,24 @@
-(function(){
-  const buttons = document.querySelectorAll('.counterBtn')
-  let count= 0
+let count = 0;
+let prev = document.querySelector(".prevBtn");
+let next = document.querySelector(".nextBtn");
+let counter = document.querySelector("#counter");
 
-
-  //Add event listeners and functionailty to each button  
-  buttons.forEach(function(button){
-    button.addEventListener('click', function(){
-      if (button.classList.contains('prevBtn')){
-        count--
-      } else if (button.classList.contains('nextBtn')){
-        count++
-      }
-
-      //Select the counter text
-      const counter = document.querySelector('#counter')
-      counter.textContent = count
-
-      if (count < 0 ){
-        counter.style.color = 'red'
-      } else if (count > 0){
-        counter.style.color = 'green'
-      } else {
-        counter.style.color = '#333333'
-      }
-    })
-  })
-})()
+function checkValue() {
+  if (count> 0) {
+    counter.style.color = "green";
+  } else if (count < 0) {
+    counter.style.color = "red";
+  } else {
+    counter.style.color = "#333333";
+  }
+}
+prev.addEventListener("click", function () {
+  count--;
+  checkValue();
+  counter.innerHTML = count;
+});
+next.addEventListener("click", function () {
+  count++;
+  checkValue();
+  counter.innerHTML = count;
+});
